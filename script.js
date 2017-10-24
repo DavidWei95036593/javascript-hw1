a7e1d74c4a242c13802fe4d6d3a04dbfafb83e9c
DavidWei95036593 <dw36593@pausd.us>
2017-10-24 09:14:11 -0700

    make the functions

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ var buttonone = document.getElementById("second");
 buttonone.addEventListener("onclick",function(){
 	var list = document.getElementById("list");
 	toggleColor(list);})
+
+
+
+function makeBorder(element){
+	var border = element.className;
+	if(border =="border")
+		element.className = "border";
+	else{
+		element.className = "noborder";
+	}
+}
+var buttontwo = document.getElementById("first");
+buttontwo.addEventListener("onclick",function(){
+	var paragraph = document.getElementById("text");
+	makeBorder(paragraph);
+})
