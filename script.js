9766b72d1eeac239ebec587ce32b462710a4d6a8
DavidWei95036593 <dw36593@pausd.us>
2017-10-24 14:46:08 -0700

    creates functions

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 function toggleColor(element){
 	var color = element.className
 	if(color ==="red")
-	{element.className = "green";}
+	{element.className = "green";
+	}
 	else{
 		element.className = "red";
-	}	
+	}
 }
 
-var buttonone = document.getElementById("second");
-buttonone.addEventListener("onclick",function(){
-	var list = document.getElementById("list");
-	toggleColor(list);})
+// var buttonone = document.getElementById("second");
+// buttonone.addEventListener("onclick",function(){
+// 	var list = document.getElementById("list");
+// 	toggleColor(list);})
 
-
+var buttontwo = document.getElementById("second");
+buttontwo.addEventListener("onclick",console.log("hi"))
 
 function makeBorder(element){
 	var border = element.className;
-	if(border =="border")
-		element.className = "border";
-	else{
+	if(border === "border")
 		element.className = "noborder";
+
+	else{
+		element.className = "border";
 	}
 }
-var buttontwo = document.getElementById("first");
-buttontwo.addEventListener("onclick",function(){
+var buttonone = document.getElementById("first");
+buttonone.addEventListener("onclick",function(){
 	var paragraph = document.getElementById("text");
 	makeBorder(paragraph);
 })
