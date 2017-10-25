a291c859d5f891c58594d15a2306571465203ad6
DavidWei95036593 <dw36593@pausd.us>
2017-10-24 17:03:13 -0700

    fix the function so it works

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
+function makeBorder(element){
+	var border = element.className;
+	if(border === "border")
+		element.className = "noborder";
+
+	else{
+		element.className = "border";
+	}
+}
+
+function changeButtononeContext(element){
+	var buttonone = element.className;
+	if(buttonone ==="first"){
+		element.innerHTML = "Add Border";
+		buttonone ==="notfirst";
+	}else{
+		element.innerHTML = "Remove Border";
+		buttonone ==="first";
+	}
+}
+
+
 function toggleColor(element){
-	var color = element.className
+	var color = element.className;
 	if(color === "red")
 	{element.className = "green";
 	}
@@ -10,30 +32,16 @@ function toggleColor(element){
 
 var buttonone = document.getElementById("second");
 buttonone.addEventListener("click",function(){
-	var list = document.getElementById("green");
-	toggleColor(list);})
-
-var buttontwo = document.getElementById("second");
-buttontwo.addEventListener("click",function(){console.log("hi")});
-
-function makeBorder(element){
-	var border = element.className;
-	if(border === "border")
-		element.className = "noborder";
-
-	else{
-		element.className = "border";
-	}
-}
-function changeButtononeContext(element){
-	var buttonone = element.className;
-	if(butonone ==="first"){
-
-	}
-}
-var buttonone = document.getElementById("first");
-buttonone.addEventListener("click",function(){
-	var paragraph = document.getElementById("noborder");
-	makeBorder(paragraph);
-
+	var list = document.getElementById("notgreen");
+	toggleColor(list);
 })
+
+var buttonone = document.getElementById("first");
+	buttonone.addEventListener("click",function(){
+		var paragraph = document.getElementById("noborder");
+		var context = document.getElementById("first");
+		changeButtononeContext(context);
+	makeBorder(paragraph);
+})
+
+	
